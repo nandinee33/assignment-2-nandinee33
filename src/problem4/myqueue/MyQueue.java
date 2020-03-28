20d28edda2b5ae3565e0842235336e37a972cd64
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-03-28 17:27:55 +0530

    make getPreSucc method to get the PreSuccessor elements

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -41,6 +41,13 @@ public class MyQueue {
         }
     }
 
+    public void getPreSucc() {
+        Node temp = front.getNext();
+        while (temp != null) {
+            System.out.print(temp.getData() + "\t");
+            temp = temp.getNext();
+        }
+    }
 
     class Node {
 
