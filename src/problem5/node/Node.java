47b3f767e67068f220666be5dd42051d5db41513
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-03-28 17:35:35 +0530

    make getter and setter of these fields

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -1,8 +1,8 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
+ *  User: nandinee
+ *  Date: 28-Mar-20
+ *  Time: 5:15 PM
  */
 package problem5.node;
 
@@ -13,5 +13,21 @@ import problem5.student.Student;
 public class Node {
     Student student;
     Node next;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 
 }
