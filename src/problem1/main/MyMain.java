fccb15c42afe01624ed2dfa819cfc07ae4c66bb2
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-03-28 15:49:21 +0530

    make object of MyBinarySearchTree

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,10 +7,12 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
 
     public static void main(String[] args) {
-
+        MyBinarySearchTree obj = new MyBinarySearchTree();
 
     }
 
