847c5577d8b1fd40ce8aee76240f0fbe2c05884c
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-03-28 17:03:15 +0530

    insert the values and then display

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -12,7 +12,12 @@ import problem3.myqueue.MyPriorityQueue;
 public class MyMain {
     public static void main(String[] args) {
         MyPriorityQueue obj = new MyPriorityQueue();
-
+        obj.insert("A", 1);
+        obj.insert("B", 4);
+        obj.insert("C", 3);
+        obj.insert("D", 2);
+        obj.insert("E", 5);
+        obj.display();
     }
 
 }
