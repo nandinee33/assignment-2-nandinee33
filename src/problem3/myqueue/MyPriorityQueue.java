910536a36d76c95e86298c8d41b90ddca4cd1e3e
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-03-28 17:01:20 +0530

    make the display method which displays the name and rollno

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -36,4 +36,12 @@ public class MyPriorityQueue {
             }
         }
     }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.getName() + "\t" + temp.getRoll());
+            temp = temp.getNext();
+        }
+    }
 }
