29783bf193ae44f2ef406294180bea226b29546f
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-03-28 17:29:42 +0530

    make object of MyBinarySearchTree and insert elements in it

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -3,9 +3,18 @@ package problem4.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
     public static void main(String[] args) {
-
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(10);
+        obj.insert(3);
+        obj.insert(30);
+        obj.insert(37);
+        obj.insert(13);
+        obj.insert(1);
+        obj.insert(7);
     }
 
 }
