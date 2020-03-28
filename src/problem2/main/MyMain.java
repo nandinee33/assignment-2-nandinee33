9398260249f541ad09f9ade8e986826dfd0215bc
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-03-28 16:48:15 +0530

    make object of MyBinarySearchTree

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -1,13 +1,16 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:15 PM
+ *  User: nandinee
+ *  Date: 28-Mar-20
+ *  Time: 4:00 PM
  */
 package problem2.main;
 // executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+import problem1.mybst.MyBinarySearchTree;
 
 public class MyMain {
-
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+    }
 }
